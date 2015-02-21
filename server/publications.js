64ac6a127f6ab3b7c61ee0a74147c936b8d97d96
Brylie Christopher Oxley <brylie@gnumedia.org>
2015-02-21 21:47:38 +0200

    Added single post publication.

--- server/publications.js
+++ server/publications.js
@@ -26,3 +26,7 @@ Meteor.publish('lazyload-posts', function (limit) {
         sort: {timeCreated: -1}
     });
 });
+
+Meteor.publish('single-post', function (slug) {
+    return Posts.find({slug: slug});
+});
