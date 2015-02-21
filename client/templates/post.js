8ce4319ea0441fcfb7c29f38114db1688de192a9
Brylie Christopher Oxley <brylie@gnumedia.org>
2015-02-22 00:01:36 +0200

    Template autorun.

---
+++ client/templates/post.js
@@ -0,0 +1,8 @@
+Template.post.rendered = function () {
+    this.autorun(function () {
+        console.log(Session.get('mySessionExample'));
+        console.log(this);
+        console.log(Template.instance());
+        console.log(Template.instance().find('p'));
+    });
+};
