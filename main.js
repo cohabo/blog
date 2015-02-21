0fd992212360459fac74521325ee9db02d728bf4
Brylie Christopher Oxley <brylie@gnumedia.org>
2015-02-22 00:01:53 +0200

    Created, working with Tracker.autorun.

---
+++ main.js
@@ -0,0 +1,8 @@
+if (Meteor.isClient) {
+//    Tracker.autorun(function (computation) {
+//        var example = Session.get('mySessionExample');
+//        if (!computation.firstRun) {
+//            console.log(someText);
+//        }
+//    });
+}
